dff5a7d7474d5f9a8c2b41b5367a179efbb7010b
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2020-05-18 00:54:28 +0300

    Конечная версия

--- main.cc
+++ main.cc
@@ -1,61 +1,94 @@
 #include "Hash_H.h"
 #include "queue.h"
 #include <assert.h>
-int ARR_SIZE = 100;
 
+const int SUCCESS = 1;
+const int FAIL = -1;
+const int HIT_CASH = 1;
+const int MISS_CASH = 0;
+
+
+//Подсчет попаданий в кэш
 int check(int value, int cash_len, struct list_d* hash_t,struct node_t* queue, int* size, int quest) {
 
-	struct list_d* node;
-	int* adress_value = find_hash(value, hash_t, cash_len);
+	// Проверка данных
+	assert(hash_t);
+	assert(queue);
+	assert(size);
 
-	if (check_in_hash(value, hash_t, cash_len) == 1) {
-		//Если в хэшэ, то ищем в списке и увеличиваем частоту
-		assert(adress_value);
-		Incr_freq(queue,value,*size); // Нашел в хэшэ ячейку со значением и передали значение в функцию, которая увеличила
-		return 1;
+	// Проверяем наличие в кэшэ
+	if (check_in_hash(value, hash_t, cash_len) == SUCCESS) {
+
+		//Если в кэшэ, то увеличиваем частоту
+		Incr_freq(queue,value,*size);
+		return HIT_CASH;
+
 	}
 	else
-		// Если не в списке, то проверяем заполненость хэша и если заполнен, то удаляем и добавляем. Если нет то добавляем в хэш и добавляем ячейку, увеличивая размер заполнености
+
+		// Добавляем в кэш
 		if (*size < cash_len) {
 			// Добавить ячейку с нужным значением
 			add_hash(hash_t, cash_len, value);
-			adress_value = find_hash(value, hash_t, cash_len);
-			// Создать привязку к частоте
+
+			// Добавить частоту
 			Incr_freq(queue, value, *size);
+
+			// Увеличиваем размер кэша
 			*size+=1;
+
+			return MISS_CASH;
 		}
+	// Удаляем из кэша и добавляем новый элемент
 		else {
 			// Удалить ячейку с наименьшим приорететом
-			hash_del(hash_t, Find_min(queue, *size), cash_len); // Нужна функция Find_min, чтобы я из хэша удалил ячейку
-			// Добавить ячейку с нужным значением
+			hash_del(hash_t, Find_min(queue, *size), cash_len);
 
+			// Добавить ячейку с нужным значением
 			add_hash(hash_t, cash_len, value);
-			adress_value = find_hash(value, hash_t, cash_len);
+
+			// Увеличить частоту
 			Incr_freq(queue,value, *size);
+
+			return MISS_CASH;
 		}
 
-	return 0;
+	return MISS_CASH;
 };
 
-int main(void) {
-	int cash_len, quest, value;
+int main(int argc, char** argv) {
+	
+	//Вводим нужные переменные
+	int cash_len = 0;
+	int quest = 0;
+	int value = 0;
 	int count = 0;
-	struct list_d* hash_t;
+	struct list_d* hash_t = NULL;
 	int size = 0;
 	
 	// Читаем длину кэша и кол-во запросов
-	scanf("%d%d", &cash_len, &quest);
+	assert(scanf("%d%d", &cash_len, &quest));
+
+	FILE* f = fopen("test.txt", "r");
 
 	//Создали очередь из кол-ва запросов
 	struct node_t* queue =  create_queue(ARR_SIZE);
+	assert(queue);
 
 	// Создаем пустую хэш таблицы
 	create_hash(cash_len, &hash_t);
 
+
+	// Запускаем кэширование запросов
 	for (int i = 0; i < quest;i++) {
-		scanf("%d", &value);
+		// Читаем запрос
+		assert(scanf("%d", &value));
+
+		// Проверяем попадание в кэш
 		if (check(value,cash_len,hash_t,queue,&size,quest) == 1)
 			count++;
 	}
+
+	// Выводим ответ
 	printf("%d", count);
 }
