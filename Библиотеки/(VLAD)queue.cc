57183da055569ceec0d460fbb88bb56c7e2f81e6
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2020-05-18 01:21:26 +0300

    Функции кучи (Влад)

--- Библиотеки/(VLAD)queue.cc
+++ Библиотеки/(VLAD)queue.cc
@@ -14,15 +14,26 @@ static inline int left(int i) { return 2 * i + 1; };
 
 static inline int right(int i) { return 2 * i + 2; };
 
+const int ARR_SIZE = 100;
+const int NO_HEAP = -1;
+const int IN_HEAP = 1;
+const int ADD_SIZE = 1000;
+
+// Создаем очередь
 struct node_t* create_queue(int N)
 {
 	struct node_t* top = (struct node_t*)calloc(N, sizeof(struct node_t));
 	return top;
 };
 
+//Ищем размер кучи
 int size_check(struct node_t* top, int data_t, int N, int* sizet)
 {
-	int place = -1;
+	int place = NO_HEAP;
+
+	assert(sizet != NULL);
+	assert(top != NULL);
+
 	for (int i = 0; i < N; i++)
 	{
 		if ((((top + i)->data) == data_t) && (((top + i)->age) != 0))
@@ -38,14 +49,19 @@ int size_check(struct node_t* top, int data_t, int N, int* sizet)
 	return place;
 };
 
+// Ищем минимальный элемент
 struct node_t* min_t(struct node_t* left, struct node_t* right)
 {
+	assert(left != NULL);
+	assert(right != NULL);
+
 	if ((left->age) <= (right->age))
 		return left;
 	else
 		return right;
 };
 
+// Меняем местами два элемента
 void swap(struct node_t* a, struct node_t* b)
 {
 	struct node_t per = *a;
@@ -81,6 +97,11 @@ void shift_up(struct node_t* top, int i)
 	if (i == 0)
 		return;
 	int new_i = parent(i);
+
+	assert((new_i > 0) && "shift_up");
+	assert(((top + i) != NULL) && "shift_up");
+	assert(((top + new_i) != NULL) && "shift_up");
+
 	if (((top + i)->age) < ((top + new_i)->age))
 	{
 		swap(top + i, (top + new_i));
@@ -88,50 +109,50 @@ void shift_up(struct node_t* top, int i)
 	};
 };
 
-/*void delete_min(struct node_t* top, int N)
-{
-swap(top, (top + N - 1));
-((top + N - 1)->age) = 0;
-((top + N - 1)->data) = NULL;
-};*/
+// Добавляем элемент в очередь
+void push(struct node_t* top, int i, int data_t) {
 
-/*int check(struct node_t* top, int* data_t, int sizet)
-{
-for (int i = 0; i < sizet; i++)
-{
-if (*((top + i)->data) == *data_t)
-return i;
-};
-return (-10);
-};*/
 
-void push(struct node_t* top, int i, int data_t)
-{
+	assert((top != NULL) && "push");
+	assert((i >= 0) && "push");
+
+
 	((top + i)->age)++;
 	((top + i)->data) = data_t;
 };
 
-void Incr_freq(struct node_t* top, int data_t, int N)
-{
-	static int size = 100;
-	int sizet = -1;
+// Добавления элемента в очередь или увеличение возраста
+void Incr_freq(struct node_t* top, int data_t, int N) {
+
+	//Задаем начальный размер кучи
+	static int size = ARR_SIZE;
+
+	//Задаем длину кучи
+	int sizet = NO_HEAP;
+
+	// Узнаем нахождение в куче
 	int p = size_check(top, data_t, size, &sizet);
-	if (p == -1)
-	{
+
+	//Выполняем добавление или увеличение
+	if (p == NO_HEAP) {
+
+		// Добавление
 		if (sizet < size)
 		{
 			push(top, sizet, data_t);
 			shift_up(top, sizet);
 		}
+		// Добавление и удлинение массива
 		else
 			if (sizet == size)
 			{
-				size = size + 1000;
+				size = size + ADD_SIZE;
 				top = (struct node_t*)realloc(top, size);
 				push(top, sizet, data_t);
 				shift_up(top, sizet);
 			};
 	}
+	// Обновление возраста
 	else
 	{
 		((top + p)->age)++;
@@ -140,7 +161,9 @@ void Incr_freq(struct node_t* top, int data_t, int N)
 	};
 };
 
+// Поиск минимального
 int Find_min(struct node_t* top, int N)
 {
+	assert((top != NULL) && "Find_min");
 	return top->data;
 };
