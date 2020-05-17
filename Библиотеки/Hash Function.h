e0b204a5ee0aa91c0d6a68bacdedf06929b73a57
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2020-05-18 00:55:57 +0300

    Библиотека хэша

--- Библиотеки/Hash Function.h
+++ Библиотеки/Hash Function.h
@@ -5,25 +5,47 @@
 #include <assert.h>
 #pragma warning (disable:4996)
 
+// Значения по-умолчанию
+const int NO_VALUE = 0;
+const int IN_CASH = 1;
+const int OUT_CASH = 0;
 
+// Ячейка хэш таблицы
 struct list_d {
 	struct list_d* parent = NULL;
 	struct list_d* son = NULL;
 	int value = 0;
 };
 
-struct list_d create_node(struct list_d* parent);
+// Вводим ячейку и создаем к ней привязанную
+void create_node(struct list_d* parent);
 
-struct list_d del_node(struct list_d* parent);
+// Удаляем ячейку по адресу (parent)
+void del_node(struct list_d* parent);
 
+
+//Хэш функция (функция отображения)
+//Вводим число(x) и длину хэша(cash_len)
 int hash_f(int x, int cash_len);
+//Получаем шифровку
 
+// Создаем хэш таблицу
+//Вводим длину хэша(cash_len) и указатель на хэш таблицу(**hash_t)
 void create_hash(int cash_len, struct list_d** hash_t);
 
+// Удаляем элемент в хэшэ 
+//Вводим адресс хэша(hash_t), значение(value) и длину хэша(cash_len).
 void hash_del(struct list_d* hash_t, int value, int cash_len);
 
+// Добавляем элемент в хэш
+//Вводим адрес хэша(hash_t), длину хэша (cash_len), и значение(value)
 void add_hash(struct list_d* hash_t, int cash_len, int value);
 
+// Проверяем наличие в хэшэ
+//Вводим значение(value), адрес хэша(hash_t) и длину хэша(cash_len)
 int check_in_hash(int value, struct list_d* hash_t, int cash_len);
+//Выводит 1 если в хэшэ и 0 если не найден элемент
 
+//Ищем элемент в хэшэ
+//Вводим значение(value), адрес хэша(hash_t) и длину хэша(cash_len)
 int* find_hash(int value, struct list_d* hash_t, int cash_len);
